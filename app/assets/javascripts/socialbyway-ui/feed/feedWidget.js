52d6c574acd781e3965ead470ed308ad06be7309
kvikramk <vikram.k@imaginea.com>
2013-02-12 15:13:31 +0530

    changes to feed and post widget

--- app/assets/javascripts/socialbyway-ui/feed/feedWidget.js
+++ app/assets/javascripts/socialbyway-ui/feed/feedWidget.js
@@ -11,26 +11,25 @@
   $.widget("ui.FeedWidget", {
     _create: function () {
       var self = this;
-      self.tabsDiv = $('<div/>').attr('class', "tabs sbw-feed-widget-" + self.options.theme);
-      self.tabsUl = $('<ul/>').attr("class", "tabs-ul");
-      self.element.append(self.tabsDiv);
-      self.tabsDiv.append(self.tabsUl);
-      self.postTab = $('<li/>');
-      self.postTag = $('<a/>').attr({
-        'class': "tab-1"
-      }).html("<span>Post</span>");
-      self.postTab.append(self.postTag);
-      self.tabsUl.append(self.postTab);
-      self.postTabDiv = $('<div/>').attr({
-        'class': "tab-content"
-      });
-      self.postTabDiv.insertAfter(self.tabsUl);
-      self.containerDiv = $('<div/>').attr('class', 'sbw-feed-container');
-      self.commentsDiv = $('<div/>').attr('class', 'comment-container');
-      self.postTabDiv.append(self.containerDiv);
-      self.postTabDiv.after(self.commentsDiv);
+      self.serviceFactory = SBW.Singletons.serviceFactory;
+      //UI TAB
+      self.$tabsDiv = $('<div/>').addClass("tabs sbw-feed-widget-" + self.options.theme);
+      self.$tabsUl = $('<ul/>').addClass("tabs-ul");
+      self.element.append(self.$tabsDiv);
+      self.$tabsDiv.append(self.$tabsUl);
+      self.$postTab = $('<li/>');
+      self.$postTag = $('<a/>').addClass('tab-1').html("<span>Post</span>");
+      self.$postTab.append(self.$postTag);
+      self.$tabsUl.append(self.$postTab);
+      //
+      self.$postTabDiv = $('<div/>').addClass('tab-content');
+      self.$postTabDiv.insertAfter(self.$tabsUl);
+      self.$containerDiv = $('<div/>').addClass('sbw-feed-container');
+      self.$commentsDiv = $('<div/>').addClass('comment-container');
+      self.$postTabDiv.append(self.$containerDiv);
+      self.$tabsUl.before(self.$commentsDiv);
 
-      self.input = $('<textarea/>').attr({
+      self.$input = $('<textarea/>', {
         name: 'comment',
         'class': 'post-box',
         maxlength: 5000,
@@ -38,24 +37,25 @@
         rows: 8,
         placeholder: 'Write here....'
       }).on('keyup', this, function () {
-        self.charsleft.html(this.value.length);
+        self.$charsleft.html(this.value.length);
       });
-      self.charsleft = $("<p/>").attr({
-        'class': 'chars-left'
-      }).text('0');
-      self.containerDiv.append(self.input);
-      self.postBtn = $('<button/>').attr({
-        'class': 'post-btn'
-      }).text("publish");
-      self.checkBoxesDiv = $('<div/>').attr('class', 'checkbox-container');
+      self.$charsleft = $("<p/>").addClass('chars-left').text('0');
+
+      self.$containerDiv.append(self.$input);
+      self.$postBtn = $('<button/>').addClass('post-btn').text("publish");
+      self.$checkBoxesDiv = $('<div/>').addClass('checkbox-container');
 
       self.options.services.forEach(function (value) {
-        var temp = $('<div/>').attr({
-          "class": "checkbox " + value
-        }).append("<input type='checkbox' name='service' value='" + value + "'/><div class='userimage'></div><div class='service-container " + value + "'></div>");
-        self.checkBoxesDiv.append(temp);
+        var temp = $('<div/>').addClass("checkbox " + value).
+        append("<input type='checkbox' name='service' value='" + value + "'/>").
+        append("<div class='userimage'></div>").
+        append("<div class='service-container " + value + "'></div>");
+        self.$checkBoxesDiv.append(temp);
       });
-      self.checkBoxesDiv.append(self.postBtn).append(self.charsleft).append('<div class="clear"></div>');
+      self.$checkBoxesDiv.append(self.$postBtn).
+      append(self.$charsleft).
+      append('<div class="clear"></div>');
+
       var successCallback = function (data) {
           var temp = '',
             date = '',
@@ -63,31 +63,34 @@
           data.forEach(function (value) {
             date = new Date(value['createdTime']);
             text = date.toDateString();
-            temp = '<div class="comments"><img class="uimg" width="50" height="50" src="' + value['picUrl'] + '"/>' + '<p class="details"><span class="name">' + value['fromUser'] + '</span><span class="time">' + text + '</span></p>' + '<p class="message">' + value['text'] + '</p>' + '<p class="likes">Like/Favorite  ' + value['likeCount'] || "0" + '</p></div>';
-            self.commentsDiv.append(temp);
+            temp = '<div class="comments"><img class="uimg" width="50" height="50" src="' + value['picUrl'] + '"/>' + 
+                   '<p class="details"><span class="name">' + value['fromUser'] + '</span><span class="time">' + text + '</span></p>' +
+                   '<p class="message">' + value['text'] + '</p>' + '<p class="likes">Like/Favorite  ' + value['likeCount'] || "0" + '</p></div>';
+            self.$commentsDiv.append(temp);
           });
         },
         failureCallback = function () {};
-      SBW.Singletons.serviceFactory.getService('controller').getCommentsForUrl(self.options.services, {
+
+      self.serviceFactory.getService('controller').getCommentsForUrl(self.options.services, {
         url: self.options.id,
         limit: self.options.limit,
         offset: self.options.offset
       }, successCallback, failureCallback);
-      self.checkBoxesDiv.insertAfter(self.input);
-      self.postBtn.on("click", this, this._addPost);
-      self.containerDiv.find(".checkbox-container").on('click', '.checkbox input', function () {
+      self.$checkBoxesDiv.insertAfter(self.$input);
+      self.$postBtn.on("click", this, this._addPost);
+      self.$containerDiv.find(".checkbox-container").on('click', '.checkbox input', function () {
         var value = this.value;
         if ($(this).is(":checked")) {
-          self.checkBoxesDiv.find(".service-container." + value).addClass('selected');
-          SBW.Singletons.serviceFactory.getService(value).startActionHandler(function () {
-            SBW.Singletons.serviceFactory.getService("controller").getProfilePic([value], null, function (response) {
+          self.$checkBoxesDiv.find(".service-container." + value).addClass('selected');
+          self.serviceFactory.getService(value).startActionHandler(function () {
+            self.serviceFactory.getService("controller").getProfilePic([value], null, function (response) {
               if (response) {
-                self.checkBoxesDiv.find('.' + value + " .userimage").css('background', 'url(' + response + ')');
+                self.$checkBoxesDiv.find('.' + value + " .userimage").css('background', 'url(' + response + ')');
               }
             }, function (error) {});
           });
         } else {
-          self.checkBoxesDiv.find(".service-container." + value).removeClass('selected');
+          self.$checkBoxesDiv.find(".service-container." + value).removeClass('selected');
         }
       });
 
@@ -106,26 +109,26 @@
     },
     _addPost: function (e) {
       var self = e.data,
-        postText = self.input.val(),
-        ServiceArr = [],
+        postText = self.$input.val(),
+        serviceArr = [],
         successCallback = function (response) {
-          var elem = self.containerDiv.find(".sbw-success-info");
+          var elem = self.$containerDiv.find(".sbw-success-info");
           if (elem.length !== 0) {
             elem.html(elem.text().substr(0, elem.text().length - 1) + ", " + response.service + ".");
           } else {
-            self.containerDiv.append('<span class="sbw-success-info">Successfully posted on ' + response.service + '.</span>');
+            self.$containerDiv.append('<span class="sbw-success-info">Successfully posted on ' + response.service + '.</span>');
           }
         },
         failureCallback = function (response) {
-          self.containerDiv.append('<span class="sbw-success-info">Some problem in posting with ' + (response.service) + '.</span>');
+          self.$containerDiv.append('<span class="sbw-success-info">Some problem in posting with ' + (response.service) + '.</span>');
         };
-      self.checkBoxesDiv.find("input:checked").each(function () {
-        ServiceArr.push(this.value);
+      self.$checkBoxesDiv.find("input:checked").each(function () {
+        serviceArr.push(this.value);
       });
-      self.containerDiv.find(".sbw-success-message").remove();
-      self.containerDiv.find(".sbw-error-message").remove();
+      self.$containerDiv.find(".sbw-success-message").remove();
+      self.$containerDiv.find(".sbw-error-message").remove();
 
-      SBW.Singletons.serviceFactory.getService("controller").publishMessage(ServiceArr, postText, successCallback, failureCallback);
+      self.serviceFactory.getService("controller").publishMessage(serviceArr, postText, successCallback, failureCallback);
 
     }
   });
--- app/assets/javascripts/socialbyway-ui/post/postWidget.js
+++ app/assets/javascripts/socialbyway-ui/post/postWidget.js
@@ -13,23 +13,23 @@
   $.widget("ui.PostWidget", /** @lends PostWidget.prototype */ {
     _create: function () {
       var self = this;
-      self.tabsDiv = $('<div/>').attr('class', "tabs sbw-post-widget-" + self.options.theme);
-      self.tabsUl = $('<ul/>').attr("class", "tabs-ul");
-      self.element.append(self.tabsDiv);
-      self.tabsDiv.append(self.tabsUl);
-      self.postTab = $('<li/>');
-      self.postTag = $('<a/>').attr({
-        'class': "tab-1"
-      }).html("<span>Post</span>");
-      self.postTab.append(self.postTag);
-      self.tabsUl.append(self.postTab);
-      self.postTabDiv = $('<div/>').attr({
-        'class': "tab-content"
-      });
-      self.postTabDiv.insertAfter(self.tabsUl);
-      self.containerDiv = $('<div/>').attr('class', 'sbw-post-container');
-      self.postTabDiv.append(self.containerDiv);
-      self.input = $('<textarea/>').attr({
+      self.serviceFactory = SBW.Singletons.serviceFactory;
+      // Tabs UI
+      self.$tabsDiv = $('<div/>').attr('class', "tabs sbw-post-widget-" + self.options.theme);
+      self.$tabsUl = $('<ul/>').attr("class", "tabs-ul");
+      self.element.append(self.$tabsDiv);
+      self.$tabsDiv.append(self.$tabsUl);
+      self.$postTab = $('<li/>');
+      self.postTag = $('<a/>').addClass('tab-1').html("<span>Post</span>");
+      self.$postTab.append(self.postTag);
+      self.$tabsUl.append(self.$postTab);
+      // Container
+      self.$postTabDiv = $('<div/>').addClass('tab-content');
+      self.$postTabDiv.insertAfter(self.$tabsUl);
+      self.$containerDiv = $('<div/>').addClass('sbw-post-container');
+      self.$postTabDiv.append(self.$containerDiv);
+
+      self.$input = $('<textarea/>', {
         name: 'comment',
         'class': 'post-box',
         maxlength: 5000,
@@ -37,40 +37,43 @@
         rows: 8,
         placeholder: 'Write here....'
       }).on('keyup', this, function () {
-        self.charsleft.html(this.value.length);
+        self.$charsleft.html(this.value.length);
       });
-      self.charsleft = $("<p/>").attr({
-        'class': 'chars-count'
-      }).text('0');
-      self.containerDiv.append(self.input);
-      self.postBtn = $('<button/>').attr({
-        'class': 'post-btn'
-      }).text("publish");
-      self.checkBoxesDiv = $('<div/>').attr('class', 'checkbox-container');
+
+      self.$charsleft = $("<p/>").addClass('chars-count').text('0');
+      self.$containerDiv.append(self.$input);
+      self.$postBtn = $('<button/>').addClass('post-btn').text("publish");
+      self.$checkBoxesDiv = $('<div/>').addClass('checkbox-container');
+
       self.options.services.forEach(function (value) {
-        var temp = $('<div/>').attr({
-          "class": "checkbox " + value
-        }).append("<input type='checkbox' name='service' value='" + value + "'/><div class='userimage'></div><div class='service-container " + value + "'></div>");
-        self.checkBoxesDiv.append(temp);
+        var temp = $('<div/>').addClass("checkbox " + value).
+        append("<input type='checkbox' name='service' value='" + value + "'/>").
+        append("<div class='userimage'></div>").
+        append("<div class='service-container " + value + "'></div>");
+        self.$checkBoxesDiv.append(temp);
       });
-      self.checkBoxesDiv.append(self.postBtn).append(self.charsleft).append('<div class="clear"></div>');
-      self.checkBoxesDiv.insertAfter(self.input);
-      self.postBtn.on("click", this, this._addPost);
-      self.containerDiv.find(".checkbox-container").on('click', '.checkbox input', function () {
+
+      self.$checkBoxesDiv.append(self.$postBtn).
+      append(self.$charsleft).
+      append('<div class="clear"></div>');
+      self.$checkBoxesDiv.insertAfter(self.$input);
+
+      self.$postBtn.on("click", this, this._addPost);
+      self.$containerDiv.find(".checkbox-container").on('click', '.checkbox input', function () {
         var value = this.value;
         if ($(this).is(":checked")) {
-          self.checkBoxesDiv.find(".service-container." + value).addClass('selected');
-          SBW.Singletons.serviceFactory.getService(value).startActionHandler(function () {
-            SBW.Singletons.serviceFactory.getService("controller").getProfilePic([value], null, function (response) {
+          self.$checkBoxesDiv.find(".service-container." + value).addClass('selected');
+          self.serviceFactory.getService(value).startActionHandler(function () {
+            self.serviceFactory.getService("controller").getProfilePic([value], null, function (response) {
               if (response) {
-                self.checkBoxesDiv.find('.' + value + " .userimage").css('background', 'url(' + response + ')');
+                self.$checkBoxesDiv.find('.' + value + " .userimage").css('background', 'url(' + response + ')');
               }
             }, function (error) {
 
             });
           });
         } else {
-          self.checkBoxesDiv.find(".service-container." + value).removeClass('selected');
+          self.$checkBoxesDiv.find(".service-container." + value).removeClass('selected');
         }
       });
 
@@ -97,7 +100,7 @@
      * @desc Removes the widget from display
      */
     destroy: function () {
-      this.tabsDiv.remove();
+      this.$tabsDiv.remove();
       $.Widget.prototype.destroy.call(this);
     },
     /**
@@ -108,26 +111,26 @@
      */
     _addPost: function (e) {
       var self = e.data,
-        postText = $(self.input).val(),
-        ServiceArr = [],
+        postText = $(self.$input).val(),
+        serviceArr = [],
         successCallback = function (response) {
-          var elem = self.containerDiv.find(".sbw-success-message");
+          var elem = self.$containerDiv.find(".sbw-success-message");
           if (elem.length !== 0) {
             elem.html(elem.text().substr(0, elem.text().length - 1) + ", " + response.service + ".");
           } else {
-            self.containerDiv.append('<span class="sbw-success-message">Successfully posted on ' + response.service + '.</span>');
+            self.$containerDiv.append('<span class="sbw-success-message">Successfully posted on ' + response.service + '.</span>');
           }
         },
         failureCallback = function (response) {
-          self.containerDiv.append('<span class="sbw-error-message">Some problem in posting with ' + response.service + '.</span>');
+          self.$containerDiv.append('<span class="sbw-error-message">Some problem in posting with ' + response.service + '.</span>');
         };
-      self.checkBoxesDiv.find("input:checked").each(function () {
-        ServiceArr.push(this.value);
+      self.$checkBoxesDiv.find("input:checked").each(function () {
+        serviceArr.push(this.value);
       });
-      self.containerDiv.find(".sbw-success-message").remove();
-      self.containerDiv.find(".sbw-error-message").remove();
+      self.$containerDiv.find(".sbw-success-message").remove();
+      self.$containerDiv.find(".sbw-error-message").remove();
 
-      SBW.Singletons.serviceFactory.getService("controller").publishMessage(ServiceArr, postText, successCallback, failureCallback);
+      self.serviceFactory.getService("controller").publishMessage(serviceArr, postText, successCallback, failureCallback);
 
     }
   });

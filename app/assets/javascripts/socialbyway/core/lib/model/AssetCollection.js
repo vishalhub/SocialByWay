708fdbf11b169a0725426b456bae21c3d950ac46
Sharad Solanki <sharad.s@imaginea.com>
2013-02-15 15:23:10 +0530

    changed the object for serviceName property and added the likes property to the metadata

--- app/assets/javascripts/socialbyway/core/lib/model/AssetCollection.js
+++ app/assets/javascripts/socialbyway/core/lib/model/AssetCollection.js
@@ -2,15 +2,15 @@
  * @class
  * @name AssetCollection
  * @namespace SBW.Models.AssetCollection
- * @property {String} id  Id of the Asset Collection
- * @property {String} title  Title of the Asset Collection
- * @property {String} createdTime  Created time of the Asset Collection
- * @property {Object} rawData  Raw data of the Asset Collection
- * @property {String} status  Status of the Asset Collection
- * @property {Array}  assets Array of {@link SBW.Models.Asset Assets} in the Asset Collection
- * @property {Object} metadata Metadata object of the Asset Collection
- *
- * @classdesc This is Asset Collection Model Class
+ * @property {String} id  Id of the asset collection
+ * @property {String} title  Title of the asset collection
+ * @property {String} createdTime  Created time of the asset collection
+ * @property {Object} rawData  Raw data of the asset collection
+ * @property {String} status  Status of the asset collection
+ * @property {String} serviceName Service name of the asset collection
+ * @property {Array}  assets Array of {@link SBW.Models.Asset Assets} in the asset collection
+ * @property {Object} metadata Metadata object of the asset collection
+ * @classdesc This is an asset collection class
  * @constructor
  */
 SBW.Models.AssetCollection = SBW.Object.extend(/** @lends SBW.Models.AssetCollection# */{
@@ -19,36 +19,36 @@ SBW.Models.AssetCollection = SBW.Object.extend(/** @lends SBW.Models.AssetCollec
   createdTime:'',
   rawData:[],
   status:'private',
+  serviceName:null,
   assets: [],
   /**
    * @inner
    * @type {Object}
    * @desc define the AssetCollection meta data
    *
-   * @property {String} dateUpdated Date updated of the Asset Collection
-   * @property {String} dateUploaded Date uploaded of the Asset Collection
-   * @property {Number} numAssets Number of Assets in the Asset Collection
-   * @property {String} assetCollectionId Id of the Asset Collection from the service
-   * @property {String} serviceName Service name of the Asset Collection
-   * @property {Number} commentCount Comment count of the Asset Collection
-   * @property {String} Type type of the Asset Collection
-   * @property {String} tags Tags of the Asset Collection
-   * @property {String} fileName File name of the Asset Collection
-   * @property {String} description Description of the Asset Collection
-   * @property {String} thumbnail Thumbnail of the Asset Collection
-   * @property {String} previewUrl Preview url of the Asset Collection
-   * @property {String} author Author of the Asset Collection
-   * @property {String} authorAvatar Author avatar of the Asset Collection
+   * @property {String} dateUpdated Date updated of the asset collection
+   * @property {String} dateUploaded Date uploaded of the asset collection
+   * @property {Number} numAssets Number of Assets in the asset collection
+   * @property {String} assetCollectionId Id of the asset collection from the service
+   * @property {String} Type type of the asset collection
+   * @property {String} tags Tags of the asset collection
+   * @property {String} fileName File name of the asset collection
+   * @property {String} description Description of the asset collection
+   * @property {String} thumbnail Thumbnail of the asset collection
+   * @property {String} previewUrl Preview url of the asset collection
+   * @property {String} author Author of the asset collection
+   * @property {String} authorAvatar Author avatar of the asset collection
+   * @property {Number} commentCount Comment count of the asset collection
+   * @property {Object} comments Comments on an asset collection
+   * @property {Number} likeCount like count of an asset collection
+   * @property {Array} likes like object of an asset collection
    *
    **/
   metadata:{
-    
     dateUpdated:null,
     dateUploaded:null,
     numAssets:null,
     assetCollectionId:null,
-    serviceName:null,
-    commentCount:null,
     type:null,
     tags:null,
     fileName:null,
@@ -56,11 +56,15 @@ SBW.Models.AssetCollection = SBW.Object.extend(/** @lends SBW.Models.AssetCollec
     thumbnail:null,
     previewUrl:null,
     author:null,
-    authorAvatar:null
+    authorAvatar:null,
+    commentCount:null,
+    comments:null,
+    likeCount:0,
+    likes:null
   },
   /**
    * @method
-   * @desc Initialize the properties of the Asset Collection
+   * @desc Initialize the properties of the asset collection
    * @param {prop} property of the object
    */
   init:function (prop)
@@ -71,7 +75,7 @@ SBW.Models.AssetCollection = SBW.Object.extend(/** @lends SBW.Models.AssetCollec
   },
   /**
    * @method
-   * @desc utility method to generate the id of Asset Collection object
+   * @desc utility method to generate the id of asset collection object
    * @returns {string} id
    */
   getID:function ()
